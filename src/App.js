3299e91e37aa27b7cf9c20bd65a9e8845aab6033
Shin Ochi <90932673+sochi2122@users.noreply.github.com>
2022-01-04 19:14:39 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [state, setState] = useState({
+    skills: [{ skill: "JavaScript", level: 4 }]
+  });
+
+  function addSkill() {
+    alert("ADD SKILL CLICKED");
+  }
+
+  return (
+    <section>
+      <h2>DEV SKILLS</h2>
+      <hr />
+      {state.skills.map((s) => (
+        <article key={s.skill}>
+          <div>{s.skill}</div> <div>{s.level}</div>
+        </article>
+      ))}
+      <hr />
+      <form>
+        <label>
+          <span>SKILL</span>
+          <input name="skill" />
+        </label>
+        <label>
+          <span>LEVEL</span>
+          <select name="level">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <button>ADD SKILL</button>
+      </form>
+    </section>
+  );
+}
